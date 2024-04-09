996cc1a8babd415cb1cef1dade6f942cdc876b61
Tayyab Kamran Sami <115224619+Tayyabsami1@users.noreply.github.com>
2024-04-10 01:14:34 +0500

    post request added

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const config = {
     user: "tks",
     password: "1234",
     server: "TAYYAB-PC1234",
-    database: "ATM",
+    database: "SocialMediaDB",
     options: {
         trustServerCertificate: true,
         trustedConnection: false,
@@ -22,35 +22,69 @@ const config = {
     port: 1433
 }
 
-app.get('/Card', async (req, res) => {
+// ! Commented this because no longer using this DB
+// app.get('/Card', async (req, res) => {
+//     try {
+//         const pool = await sql.connect(config);
+//         const data = pool.request().query('select * from Card');
+//         data.then(res1 => {
+//             console.log(res1);
+//             return res.json(res1);
+//         })
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+
+// })
+
+// ! This is also related to old db
+// TODO Make a youtube video about it 
+// app.get('/card/:cardNum', async (req, res) => {
+//     try {
+//         const pool = await sql.connect(config);
+//         const data = pool.request().input('parameter', sql.VarChar, req.params.cardNum).query("select * from card where cardNum=@parameter");
+
+//         data.then(res1 => {
+//             return res.json(res1.recordset);
+//         }
+//         )
+
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+// })
+
+// Sample Json data for the code below 
+// {
+//     "Username":"tayyab4",
+//     "Password":"1234"
+//   }
+app.post('/Signup', async (req, res) => {
+
     try {
+
+        if (!req.body.Username || !req.body.Password) {
+            return res.status(400).json({ msg: 'Missing required fields: Username and Password' });
+        }
+
         const pool = await sql.connect(config);
-        const data = pool.request().query('select * from Card');
-        data.then(res1 => {
-            console.log(res1);
-            return res.json(res1);
-        })
+
+        const myreq =  pool.request();
+        myreq.input("Username", sql.VarChar(50), req.body.Username);
+        myreq.input("Password", sql.VarChar(50), req.body.Password);
+
+        const data = await myreq.query("insert into Users (Username,Password) values (@Username,@Password);")
+
+            return res.json({ msg: 'User added successfully' });
     }
     catch (err) {
         console.log(err);
     }
 
-})
+});
 
-app.get('/card/:cardNum', async (req, res) => {
-    try {
-        const pool = await sql.connect(config);
-        const data = pool.request().input('parameter', sql.VarChar, req.params.cardNum).query("select * from card where cardNum=@parameter");
-        data.then(res1 => {
-            return res.json(res1.recordset);
-        }
-        )
-
-    }
-    catch (err) {
-            console.log(err);
-    }
-})
 
 app.get('/', (req, res) => {
     return res.json("Hi i am backend");
