b2317ed4de301856f626ed25d00dc3d5542f9f40
Saad7912 <125139530+Saad7912@users.noreply.github.com>
2024-05-04 08:59:45 -0700

    Update messages.js
    The Routes/message.js file plays a crucial role as the middleware in facilitating communication between the frontend and backend of an application.

--- Backend/Routes/messages.js
+++ Backend/Routes/messages.js
@@ -1,9 +1,20 @@
-import Express from "express"
-import {  } from "../Controllers/message.js";
+// Backend/routes/message.js
+import express from 'express';
+import { getFriends, getMessages, sendMessage } from '../Controllers/message.js';
 
-const router=Express.Router();
+const router = express.Router();
 
+// Get all friends for a user
+// router.get('/friends', getFriends);
+router.get('/friends/:userId', getFriends);
 
-router.get("", );
+// Get messages between the current user and a friend
+// router.get('/friends/:userId/:friendId', getMessages);
+router.get('/:userId/:friendId', getMessages);
+
+// Send a new message
+// This should be a POST request just to '/send' if senderId and receiverId are in the body.
+router.post('/send', sendMessage);
+
 
 export default router;
