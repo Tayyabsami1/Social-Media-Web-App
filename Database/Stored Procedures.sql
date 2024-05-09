4a412bd9426d1684d16dd63a6867c4e6f03e6b81
Abdul Ahad Tariq <abdulahadg2004@gmail.com>
2024-05-09 16:27:33 +0500

    Stored Procedures and Triggers added

---
+++ Database/Stored Procedures.sql
@@ -0,0 +1,80 @@
+CREATE PROCEDURE AddUser(
+    @p_username VARCHAR(50),
+     @p_email VARCHAR(255),
+     @p_password VARCHAR(255),
+     @p_first_name VARCHAR(50),
+     @p_last_name VARCHAR(50),
+     @p_bio TEXT,
+     @p_location VARCHAR(100),
+     @p_occupation VARCHAR(50),
+     @p_interests TEXT,
+    @p_birthdate DATE,
+     @p_profile_picture VARCHAR(255)
+)
+AS
+BEGIN
+    INSERT INTO Users (username, email, password, first_name, last_name, bio, location, occupation, interests, birthdate, profile_picture)
+    VALUES (@p_username, @p_email, @p_password, @p_first_name, @p_last_name, @p_bio, @p_location, @p_occupation, @p_interests, @p_birthdate, @p_profile_picture);
+END 
+
+CREATE PROCEDURE AddPost(
+    @p_user_id INT,
+    @p_content varchar(1000),
+    @p_post_type varchar(10),
+    @p_media_url VARCHAR(255)
+)
+AS
+BEGIN
+    INSERT INTO Posts (user_id, content, post_type, media_url)
+    VALUES (@p_user_id, @p_content, @p_post_type, @p_media_url);
+END 
+
+CREATE PROCEDURE AddComment(
+    @p_post_id INT,
+    @p_user_id INT,
+    @p_content TEXT
+)
+ AS
+BEGIN
+    INSERT INTO Comments (post_id, user_id, content)
+    VALUES (@p_post_id, @p_user_id, @p_content);
+END 
+
+CREATE PROCEDURE LikePost(
+    @p_post_id INT,
+    @p_user_id INT
+)
+AS
+BEGIN
+    INSERT INTO Likes (post_id, user_id)
+    VALUES (@p_post_id, @p_user_id);
+END
+
+
+
+
+CREATE PROCEDURE AddFriend(
+    @p_user_id_1 INT,
+    @p_user_id_2 INT
+)
+AS
+BEGIN
+    DECLARE @user_count INT;
+
+    -- Check if both users exist
+    SELECT @user_count = COUNT(*) FROM Users WHERE user_id IN (@p_user_id_1, @p_user_id_2);
+    IF (@user_count = 2)
+    BEGIN
+        -- Check if users are already friends
+        IF NOT EXISTS (
+            SELECT * FROM Friends
+            WHERE (friend_id_1 = @p_user_id_1 AND friend_id_2 = @p_user_id_2)
+            OR (friend_id_1 = @p_user_id_2 AND friend_id_2 = @p_user_id_1)
+        )
+        BEGIN
+            -- Add friendship
+            INSERT INTO Friends (friend_id_1, friend_id_2)
+            VALUES (LEAST(@p_user_id_1, @p_user_id_2), GREATEST(@p_user_id_1, @p_user_id_2));
+        END
+    END
+END;
---
+++ Database/Triggers.sql
@@ -0,0 +1,105 @@
+CREATE TRIGGER UpdateUserTimestamp
+ON Users
+AFTER UPDATE
+AS
+BEGIN
+    UPDATE Users 
+    SET updated_at = CURRENT_TIMESTAMP 
+    FROM inserted
+    WHERE Users.user_id = inserted.user_id;
+END;
+
+CREATE TRIGGER DeleteCommentsOnPostDelete
+ON Posts
+AFTER DELETE
+AS
+BEGIN
+    -- Delete comments associated with the deleted post
+    DELETE FROM Comments 
+    WHERE post_id IN (SELECT post_id FROM deleted);
+END;
+
+CREATE TRIGGER UpdatePostTimestamp
+ON Posts
+AFTER UPDATE
+AS
+BEGIN
+    -- Update the timestamp for modified posts
+    UPDATE Posts
+    SET timestamp = CURRENT_TIMESTAMP
+    FROM inserted
+    WHERE Posts.post_id = inserted.post_id;
+END;
+
+
+CREATE TRIGGER DeleteLikesOnPostDelete
+ON Posts
+AFTER DELETE
+AS
+BEGIN
+    -- Delete likes associated with the deleted post
+    DELETE FROM Likes 
+    WHERE post_id IN (SELECT post_id FROM deleted);
+END;
+
+CREATE TRIGGER CheckAgeBeforeInsert
+ON Users
+INSTEAD OF INSERT
+AS
+BEGIN
+    -- Check the age before insertion
+    IF EXISTS (
+        SELECT 1
+        FROM inserted
+        WHERE DATEDIFF(year, birthdate, GETDATE()) < 18
+    )
+    BEGIN
+        -- If age is less than 18, don't insert the record
+        RAISERROR ('User must be at least 18 years old.', 16, 1)
+        ROLLBACK TRANSACTION;
+    END
+    ELSE
+    BEGIN
+        -- If age is 18 or older, proceed with insertion
+        INSERT INTO Users (username, email, password, first_name, last_name, bio, location, occupation, interests, birthdate, profile_picture, cover_picture, created_at, updated_at)
+        SELECT username, email, password, first_name, last_name, bio, location, occupation, interests, birthdate, profile_picture, cover_picture, created_at, updated_at
+        FROM inserted;
+    END
+END;
+
+
+
+
+
+CREATE TRIGGER CheckCommentContent
+ON Comments
+INSTEAD OF INSERT
+AS
+BEGIN
+    -- Check comment content before insertion
+    IF EXISTS (
+        SELECT 1
+        FROM inserted
+        WHERE 
+            content LIKE '%Shit%'
+            OR content LIKE '%Bastard%'
+            OR content LIKE '%Bitch%'
+            OR content LIKE '%Sexy%'
+            OR content LIKE '%fat%'
+            OR content LIKE '%ugly%'
+    )
+    BEGIN
+        -- If comment contains inappropriate language, raise an error
+        RAISERROR ('Comment contains inappropriate language.', 16, 1);
+		ROLLBACK TRANSACTION;
+    END
+    ELSE
+    BEGIN
+        -- If comment is appropriate, proceed with insertion
+        INSERT INTO Comments (user_id, post_id, content, timestamp)
+        SELECT user_id, post_id, content, timestamp
+        FROM inserted;
+    END
+END;
+
+
